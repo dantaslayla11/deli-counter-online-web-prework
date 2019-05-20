e939165fc12b67993640c33a19c0ac1030d43449
layla dantas <laylatdantas@gmail.com>
2019-05-20 05:25:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,21 +1,19 @@
 def line(array)
   if array.length == 0 
     puts "The line is currently empty."
-end
-  if array.length == 3
+  end
+if array.length == 3
     puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
 end
-  if array.length == 10 
+if array.length == 10 
     puts "The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey"
 end
-
 
 def take_a_number(array, string)
    
    line = "Welcome, " + string + ". You are number " + array.length.to_s + " in line."
    puts line
    array.push(string)
-   
 end
 end
 
